69bd9044ddc65f431e74ee2b19faee7ab1ea8b34
Jack Decker <jackdeckere@gmail.com>
2020-11-28 17:27:04 -0500

    Initialized firebase

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	let name = 'world';
+</script>
+<svelte:head>
+	<!-- The core Firebase JS SDK is always required and must be listed first -->
+	<script src="/__/firebase/8.1.1/firebase-app.js"></script>
+
+	<!-- TODO: Add SDKs for Firebase products that you want to use
+		https://firebase.google.com/docs/web/setup#available-libraries -->
+	<script src="/__/firebase/8.1.1/firebase-analytics.js"></script>
+
+	<!-- Initialize Firebase -->
+	<script src="/__/firebase/init.js"></script>
+</svelte:head>
+
+<h1>Hello {name}!</h1>
